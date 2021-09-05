23ad070f91cbb44b4d39b93ca33334e144d4e030
sarablom <sara.cecilia.blom@gmail.com>
2021-09-05 13:21:51 +0200

    created a player component and installed vite

---
+++ frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Player from './components/Player'
+
+
+function App() {
+  
+
+  return (
+    <div className="App">
+      <Player videoId ='z4WCaWJgOqM' />
+      
+    </div>
+  )
+}
+
+export default App;
