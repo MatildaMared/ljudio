cb3a4e04c0faa0b0f240acfc344c796c459bc7b6
Sofia Hervéus <sofia@herveus.se>
2021-09-06 12:12:13 +0200

    Starting on signup page

--- frontend/src/pages/SignupPage.jsx
+++ frontend/src/pages/SignupPage.jsx
@@ -1,11 +1,95 @@
-import React from "react";
+import React, { useState } from 'react';
+import { useHistory, Link } from 'react-router-dom';
 
 function SignupPage() {
-	return (
-		<div>
-			<h1>Sign up</h1>
-		</div>
-	);
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [checkPassword, setCheckPassword] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    passwordIdentical(password, checkPassword);
+  };
+
+  const passwordIdentical = (pass1, pass2) => {
+    if (pass1 !== pass2) {
+      console.log(
+        `Password don't match 💥 First: '${pass1}', Second: '${pass2}'`
+      );
+    } else if (pass1 === pass2) {
+      console.log(`It's a match!! 😀 ${pass1} = ${pass2}`);
+      createAccount(firstName, lastName, email, password);
+    }
+  };
+
+  async function createAccount(firstName, lastName, email, password) {
+    try {
+      const res = await fetch('http://localhost:8000/api/auth/signup', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          firstName,
+          lastName,
+          email,
+          password,
+        }),
+      });
+
+      const data = await res.json();
+      console.log(data);
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  return (
+    <div>
+      <h1>Sign up</h1>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="text">First name: </label>
+        <input
+          type="text"
+          className="firstName"
+          name="firstName"
+          onChange={(e) => setFirstName(e.target.value)}
+        />
+        <label htmlFor="text">Last name: </label>
+        <input
+          type="text"
+          className="lastName"
+          name="lastName"
+          onChange={(e) => setLastName(e.target.value)}
+        />
+        <label htmlFor="email">Email: </label>
+        <input
+          type="email"
+          className="email"
+          type="email"
+          name="email"
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <label htmlFor="password">Password: </label>
+        <input
+          type="password"
+          name="password"
+          className="password"
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <label htmlFor="password">Password again: </label>
+        <input
+          type="password"
+          name="password"
+          className="password"
+          onChange={(e) => setCheckPassword(e.target.value)}
+        />
+        <input type="submit" className="form__btn" value="Create account" />
+      </form>
+    </div>
+  );
 }
 
 export default SignupPage;
