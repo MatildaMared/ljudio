9f8870983c6f440997bcbc91c6c28b65c32b8efe
sarablom <sara.cecilia.blom@gmail.com>
2021-09-06 15:46:23 +0200

    created a searchbar component

---
+++ frontend/src/components/SearchBar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function SearchBar() {
+    return (
+        <div>
+            <input type="submit" placeholder="Search" />
+        </div>
+    )
+}
+
+export default SearchBar
