765dcac3559eeb732e79a80c232142008e953918
sarablom <sara.cecilia.blom@gmail.com>
2021-09-07 13:37:08 +0200

    added searchbar

--- frontend/src/components/SearchBar.jsx
+++ frontend/src/components/SearchBar.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
 
 function SearchBar() {
+const searchResult = (event) => {
+    const searchString = event.target.value;
+    console.log(searchString);
+}
+
     return (
         <div>
-            <input type="submit" placeholder="Search" />
+            <input type="submit" placeholder="Search" onChange= { searchResult } />
         </div>
     )
 }
