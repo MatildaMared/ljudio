fe8c23e28398c41cbf3da3270c3d8de79aa47c37
sarablom <sara.cecilia.blom@gmail.com>
2021-09-15 12:17:04 +0200

    changed the map method to a for loop

--- frontend/src/pages/QueuePage.jsx
+++ frontend/src/pages/QueuePage.jsx
@@ -4,18 +4,28 @@ import { MusicContext } from "./../context/MusicContext";
 function QueuePage() {
     const [musicContext, updateMusicContext] = useContext(MusicContext);
     const musicQueue = musicContext.queue;
+    const nowPlayingIndex = musicContext.nowPlayingIndex;
+    let items = [];
 
-    console.log(musicQueue);
-    
+    console.log('nowPlayingindex: ', nowPlayingIndex);
+    console.log('musicqueue: ', musicQueue);
+    console.log('items before ', items);
+ 
+    for (let i = nowPlayingIndex; i < (musicQueue - nowPlayingIndex); i++) {
+        console.log('nowPlayingindex in loop: ', nowPlayingIndex);
+        console.log('musicqueue in loop: ', musicQueue);
+        items.push(<li key={nowPlayingIndex}>
+            {musicQueue.name} by {musicQueue.artist.name}
+        </li>) 
+    }
+
+    console.log('items after ', items);
+ 
     return (
         <section className="result">
 			<h1>Queue</h1>
-			<ul>
-				{musicQueue.map((item) => (
-					<li key={item.videoId}>
-						{`${item.name} by ${item.artist.name}`}
-					</li>
-				))}
+			<ul>           
+                {items}
 			</ul>
 		</section>
     )
