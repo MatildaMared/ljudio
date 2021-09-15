cbdc1ea371e799cd545fc06c9f0a9ecd499379b7
Sofia Hervéus <sofia@herveus.se>
2021-09-15 14:47:07 +0200

    Buttons for play and queue in Songs-result

--- frontend/src/components/SongsResults.jsx
+++ frontend/src/components/SongsResults.jsx
@@ -1,28 +1,54 @@
-import React, { useContext } from "react";
-import { MusicContext } from "../context/MusicContext";
+import React, { useContext, useState } from 'react';
+import { MusicContext } from '../context/MusicContext';
+import { FaPlay } from 'react-icons/fa';
+import { AiFillDatabase } from 'react-icons/ai';
 
 function SongsResults() {
-	const [musicContext, updateMusicContext] = useContext(MusicContext);
+  const [musicContext, updateMusicContext] = useContext(MusicContext);
 
-	function playSong(song) {
-		console.log(song);
-		updateMusicContext({
-			nowPlaying: song,
-		});
-	}
+  function playSong(song) {
+    if (musicContext.queue.length === 0) {
+      updateMusicContext({
+        queue: [...musicContext.queue, song],
+        nowPlayingIndex: 0,
+      });
+    } else {
+      const newPlayQueue = [...musicContext.queue];
+      newPlayQueue.splice(musicContext.nowPlayingIndex + 1, 0, song);
+      updateMusicContext({
+        queue: newPlayQueue,
+        nowPlayingIndex: musicContext.nowPlayingIndex + 1,
+      });
+    }
+  }
 
-	return (
-		<section className="result">
-			<h1>Songs</h1>
-			<ul>
-				{musicContext.fetchResult.content.map((item) => (
-					<li key={item.videoId} onClick={() => playSong(item)}>
-						{`${item.name} by ${item.artist.name}`}
-					</li>
-				))}
-			</ul>
-		</section>
-	);
+  const addToQueue = (song) => {
+    updateMusicContext({
+      queue: [...musicContext.queue, song],
+    });
+  };
+
+  return (
+    <section className="result">
+      <h1>Songs</h1>
+      <ul>
+        {musicContext.fetchResult.content.map((item) => (
+          <li key={item.videoId}>
+            <button
+              onClick={() => playSong(item)}
+              style={{ marginRight: '.3rem' }}
+            >
+              <FaPlay />
+            </button>
+            <button onClick={() => addToQueue(item)}>
+              <AiFillDatabase />
+            </button>
+            {`${item.name} by ${item.artist.name}`}
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
 }
 
 export default SongsResults;
