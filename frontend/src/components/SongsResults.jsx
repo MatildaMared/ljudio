bb79336418e685bb367a13e3d52fae3a723e848b
Sofia Hervéus <sofia@herveus.se>
2021-09-17 12:23:27 +0200

    Delete function not in use

--- frontend/src/components/SongsResults.jsx
+++ frontend/src/components/SongsResults.jsx
@@ -1,26 +1,10 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext } from 'react';
 import { MusicContext } from '../context/MusicContext';
 import PlaySongBtn from './PlaySongBtn';
 import AddToPlayQueue from '../components/AddToPlayQueue';
 
 function SongsResults() {
   const [musicContext, updateMusicContext] = useContext(MusicContext);
-
-  function playSong(song) {
-    if (musicContext.queue.length === 0) {
-      updateMusicContext({
-        queue: [...musicContext.queue, song],
-        nowPlayingIndex: 0,
-      });
-    } else {
-      const newPlayQueue = [...musicContext.queue];
-      newPlayQueue.splice(musicContext.nowPlayingIndex + 1, 0, song);
-      updateMusicContext({
-        queue: newPlayQueue,
-        nowPlayingIndex: musicContext.nowPlayingIndex + 1,
-      });
-    }
-  }
 
   return (
     <section className="result">
