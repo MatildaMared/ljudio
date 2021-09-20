55a0ce6d4ea3016dbbb502ac0626b65991182c75
MatildaMared <matildamared@live.se>
2021-09-20 14:11:10 +0200

    add sharelinkbtn component

--- frontend/src/components/ShareLinkBtn.jsx
+++ frontend/src/components/ShareLinkBtn.jsx
@@ -1,8 +1,17 @@
 import React from "react";
 import { FiShare } from "react-icons/fi";
 
-function ShareLinkBtn({className}) {
-	return <button className={`btn share-btn ${className}`}>Share Link<FiShare /></button>;
+function ShareLinkBtn({ className }) {
+	const currentUrl = window.location.href;
+	console.log(currentUrl);
+	return (
+		<>
+			<button className={`btn share-btn ${className}`}>
+				Share Link
+				<FiShare />
+			</button>
+		</>
+	);
 }
 
 export default ShareLinkBtn;
