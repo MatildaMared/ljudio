8e5a5f7cb42fd616aba648b189efb1ddedc749fc
Sofia Hervéus <sofia@herveus.se>
2021-09-23 16:47:50 +0200

    Make it possible to click anywhere outside the addtoplaylist-modal to close it

--- frontend/src/components/AddToPlaylist.jsx
+++ frontend/src/components/AddToPlaylist.jsx
@@ -1,31 +1,54 @@
-import React, { useState, useContext } from 'react';
+import React, { useState, useEffect, useRef, useContext } from 'react';
 import { MdMoreVert } from 'react-icons/md';
 import { UserContext } from '../context/UserContext';
 import { addSongToPlaylist } from '../services/playlistService';
 
 function AddToPlaylist({ item }) {
   const [userContext, updateUserContext] = useContext(UserContext);
-  const [toggle, setToggle] = useState(false);
+  const [isActive, setIsActive] = useState(false);
+  const ref = useRef();
+
+  useEffect(() => {
+    const checkIfClickedOutside = (e) => {
+      console.log('In checkClick');
+      if (isActive && ref.current && !ref.current.contains(e.target)) {
+        setIsActive(false);
+      }
+    };
+
+    document.addEventListener('mousedown', checkIfClickedOutside);
+    return () => {
+      document.removeEventListener('mousedown', checkIfClickedOutside);
+    };
+  }, [isActive]);
 
   const addSongToThisPlaylist = async (playlist, song) => {
-    console.log(`Playlist with the id: ${playlist} and song: ${song.name}`);
+    console.log(
+      `In AddToPlaylist.jsx - Playlist with the id: ${playlist} and song: ${song.name}`
+    );
     const res = await addSongToPlaylist(playlist, song);
   };
 
   return (
-    <div>
+    <div className="ref-wrapper" ref={ref}>
+      <button
+        className="add-to-playlist-modal__btn"
+        onClick={() => setIsActive((oldState) => !oldState)}
+      >
+        <MdMoreVert />
+      </button>
       <div className="add-to-playlist-modal">
         <div
           className="add-to-playlist-modal__content"
           onClick={(e) => e.stopPropagation()}
         >
           <ul className="add-to-playlist-modal__ul">
-            {toggle &&
+            {isActive &&
               userContext.user.playlists.map((playlist) => (
                 <li
                   key={playlist._id}
                   onClick={() => addSongToThisPlaylist(playlist._id, item)}
-                  className={toggle ? 'show-playlist' : 'show-playlist--hide'}
+                  className={isActive ? 'show-playlist' : 'show-playlist--hide'}
                 >
                   {playlist.title}
                 </li>
@@ -33,12 +56,6 @@ function AddToPlaylist({ item }) {
           </ul>
         </div>
       </div>
-      <button
-        className="add-to-playlist-modal__btn"
-        onClick={() => setToggle(!toggle)}
-      >
-        <MdMoreVert />
-      </button>
     </div>
   );
 }
