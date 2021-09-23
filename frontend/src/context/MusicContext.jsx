74641bfca0aac653a11dde4c5f9a57b06a7f326f
MatildaMared <matildamared@live.se>
2021-09-23 07:50:32 +0200

    remove unnecessary code

--- frontend/src/context/MusicContext.jsx
+++ frontend/src/context/MusicContext.jsx
@@ -13,11 +13,6 @@ export const MusicProvider = ({ children }) => {
 		resetPlayer: false,
 	});
 
-	useEffect(() => {
-		console.log("queue is: ", context.queue);
-		console.log("nowPlayingIndex is: ", context.nowPlayingIndex);
-	}, [context.queue, context.nowPlayingIndex]);
-
 	function updateContext(updates) {
 		setContext((prevState) => {
 			return {
--- frontend/src/pages/QueuePage.jsx
+++ frontend/src/pages/QueuePage.jsx
@@ -131,38 +131,6 @@ function QueuePage() {
 				</Droppable>
 			</DragDropContext>
 		</section>
-		// <section className="queue-page">
-		// 	<h1 className="queue-page__header">Queue</h1>
-		// 	<DragDropContext onDragEnd={handleOnDragEnd}>
-		// 		<Droppable droppableId="characters">
-		// 			{(provided) => (
-		// 				<ul
-		// 					className="characters"
-		// 					{...provided.droppableProps}
-		// 					ref={provided.innerRef}>
-		// 					{characters.map(({ id, name, thumb }, index) => {
-		// 						return (
-		// 							<Draggable key={id} draggableId={id} index={index}>
-		// 								{(provided) => (
-		// 									<li
-		// 										ref={provided.innerRef}
-		// 										{...provided.draggableProps}
-		// 										{...provided.dragHandleProps}>
-		// 										<div className="characters-thumb">
-		// 											<h1>Hej + {index}</h1>
-		// 										</div>
-		// 										<p>{name}</p>
-		// 									</li>
-		// 								)}
-		// 							</Draggable>
-		// 						);
-		// 					})}
-		// 					{provided.placeholder}
-		// 				</ul>
-		// 			)}
-		// 		</Droppable>
-		// 	</DragDropContext>
-		// </section>
 	);
 }
 
