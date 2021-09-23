00370c0ea6fa81fcb3c68fc2df2271641ec0c050
Sofia Hervéus <sofia@herveus.se>
2021-09-23 15:53:46 +0200

    add toggle on add-to-playlist-btn and styles

--- frontend/src/components/AddToPlaylist.jsx
+++ frontend/src/components/AddToPlaylist.jsx
@@ -5,39 +5,38 @@ import { addSongToPlaylist } from '../services/playlistService';
 
 function AddToPlaylist({ item }) {
   const [userContext, updateUserContext] = useContext(UserContext);
-  const [isActive, setIsActive] = useState(false);
+  const [toggle, setToggle] = useState(false);
 
   const addSongToThisPlaylist = async (playlist, song) => {
     console.log(`Playlist with the id: ${playlist} and song: ${song.name}`);
     const res = await addSongToPlaylist(playlist, song);
   };
 
-  // window.onclick = function () {
-  //   if (isActive === true) {
-  //     setIsActive(false);
-  //   }
-  // };
-
   return (
     <div>
-      <div
-        className="add-to-playlist__modal"
-        onClick={() => setIsActive(false)}
-      >
-        <ul className="add-to-playlist__modal--ul">
-          {isActive &&
-            userContext.user.playlists.map((playlist) => (
-              <li
-                key={playlist._id}
-                onClick={() => addSongToThisPlaylist(playlist._id, item)}
-                className={isActive ? 'show-playlist' : 'show-playlist--hide'}
-              >
-                {playlist.title}
-              </li>
-            ))}
-        </ul>
+      <div className="add-to-playlist-modal">
+        <div
+          className="add-to-playlist-modal__content"
+          onClick={(e) => e.stopPropagation()}
+        >
+          <ul className="add-to-playlist-modal__ul">
+            {toggle &&
+              userContext.user.playlists.map((playlist) => (
+                <li
+                  key={playlist._id}
+                  onClick={() => addSongToThisPlaylist(playlist._id, item)}
+                  className={toggle ? 'show-playlist' : 'show-playlist--hide'}
+                >
+                  {playlist.title}
+                </li>
+              ))}
+          </ul>
+        </div>
       </div>
-      <button onClick={() => setIsActive(true)}>
+      <button
+        className="add-to-playlist-modal__btn"
+        onClick={() => setToggle(!toggle)}
+      >
         <MdMoreVert />
       </button>
     </div>
--- frontend/src/components/LoginForm.jsx
+++ frontend/src/components/LoginForm.jsx
@@ -1,85 +1,85 @@
-import React, { useState, useContext } from "react";
-import { useHistory, useLocation, Link } from "react-router-dom";
-import ErrorModal from "../modals/ErrorModal";
-import { loginFetch } from "../services/authService";
-import { UserContext } from "../context/UserContext";
+import React, { useState, useContext } from 'react';
+import { useHistory, useLocation, Link } from 'react-router-dom';
+import ErrorModal from '../modals/ErrorModal';
+import { loginFetch } from '../services/authService';
+import { UserContext } from '../context/UserContext';
 
 function LoginForm() {
-	const [userContext, updateUserContext] = useContext(UserContext);
-	const [email, setEmail] = useState("");
-	const [password, setPassword] = useState("");
-	const history = useHistory();
-	const location = useLocation();
-	//Show or hide modal that displays an error message when user typed something wrong
-	const [show, setShow] = useState(false);
-	const [errorMessage, setErrorMessage] = useState("");
+  const [userContext, updateUserContext] = useContext(UserContext);
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const history = useHistory();
+  const location = useLocation();
+  //Show or hide modal that displays an error message when user typed something wrong
+  const [show, setShow] = useState(false);
+  const [errorMessage, setErrorMessage] = useState('');
 
-	function handleSubmit(e) {
-		e.preventDefault();
-		login(email, password);
-	}
+  function handleSubmit(e) {
+    e.preventDefault();
+    login(email, password);
+  }
 
-	async function login(email, password) {
-		const obj = {
-			email,
-			password,
-		};
-		try {
-			const data = await loginFetch(obj);
-			if (data.success) {
-				localStorage.setItem("token", data.token);
-				updateUserContext({
-					user: data.user,
-					isAuthenticated: true,
-				});
-				if (location.pathname === "/login") {
-					history.push("/");
-				}
-			} else {
-				setShow(true);
-				setErrorMessage(data.error);
-			}
-		} catch (err) {
-			console.log(err);
-		}
-	}
+  async function login(email, password) {
+    const obj = {
+      email,
+      password,
+    };
+    try {
+      const data = await loginFetch(obj);
+      if (data.success) {
+        localStorage.setItem('token', data.token);
+        updateUserContext({
+          user: data.user,
+          isAuthenticated: true,
+        });
+        if (location.pathname === '/login') {
+          history.push('/');
+        }
+      } else {
+        setShow(true);
+        setErrorMessage(data.error);
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  }
 
-	return (
-		<section className="loginpage">
-			<h1 className="loginpage__logo">Ljudio</h1>
-			<h2 className="loginpage__heading">Log in</h2>
-			<form className="loginpage__form" onSubmit={handleSubmit}>
-				<div className="loginpage__form-control">
-					<input
-						className="loginpage__input"
-						type="email"
-						name="email"
-						id="email"
-						onChange={(e) => setEmail(e.target.value)}
-					/>
-					<label htmlFor="email" className="loginpage__label">
-						Email
-					</label>
-				</div>
-				<div className="loginpage__form-control">
-					<input
-						className="loginpage__input"
-						type="password"
-						name="password"
-						id="password"
-						onChange={(e) => setPassword(e.target.value)}
-					/>
-					<label htmlFor="password" className="loginpage__label">
-						Password
-					</label>
-				</div>
-				<input className="loginpage__btn" type="submit" value="Log in" />
-			</form>
-			<ErrorModal title="Error" onClose={() => setShow(false)} show={show}>
-				<p>{errorMessage}</p>
-			</ErrorModal>
-		</section>
-	);
+  return (
+    <section className="loginpage">
+      <h1 className="loginpage__logo">Ljudio</h1>
+      <h2 className="loginpage__heading">Log in</h2>
+      <form className="loginpage__form" onSubmit={handleSubmit}>
+        <div className="loginpage__form-control">
+          <input
+            className="loginpage__input"
+            type="email"
+            name="email"
+            id="email"
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <label htmlFor="email" className="loginpage__label">
+            Email
+          </label>
+        </div>
+        <div className="loginpage__form-control">
+          <input
+            className="loginpage__input"
+            type="password"
+            name="password"
+            id="password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <label htmlFor="password" className="loginpage__label">
+            Password
+          </label>
+        </div>
+        <input className="loginpage__btn" type="submit" value="Log in" />
+      </form>
+      <ErrorModal title="Error" onClose={() => setShow(false)} show={show}>
+        <p>{errorMessage}</p>
+      </ErrorModal>
+    </section>
+  );
 }
 
 export default LoginForm;
