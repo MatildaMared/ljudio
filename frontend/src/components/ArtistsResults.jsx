f4b32ebdadc57330dfff113fa83a5d439a80dca2
MatildaMared <matildamared@live.se>
2021-09-27 08:57:20 +0200

    add basic styling to artistsresults

--- frontend/src/components/ArtistsResults.jsx
+++ frontend/src/components/ArtistsResults.jsx
@@ -1,27 +1,20 @@
 import React, { useContext } from "react";
 import { useHistory } from "react-router-dom";
 import { MusicContext } from "../context/MusicContext";
+import ArtistsList from "./ArtistsList";
 
 function ArtistsResults() {
 	const [musicContext, updateMusicContext] = useContext(MusicContext);
 	const history = useHistory();
-	function clickHandler (browseId) {
+	function clickHandler(browseId) {
 		history.push(`/artist/${browseId}`);
 	}
 
 	return (
-		<section className="result">
-			<h1>Artists</h1>
-			<ul>
-				{musicContext.fetchResult.content.map((item) => (
-					<li onClick={() => clickHandler(item.browseId)} key={item.browseId} style={{display: "flex", justifyContent: "space-between", alignItems: "center"}}>
-            <p>{`${item.name}`}</p>
-            <img src={item.thumbnails[0].url} alt={item.name}></img>
-          </li>
-        ))}
-      </ul>
-    </section>
-  );
+		<section className="artists-results">
+			<ArtistsList artists={musicContext.fetchResult.content} />
+		</section>
+	);
 }
 
 export default ArtistsResults;
